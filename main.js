592fd5456c5c6560aff55f517f84417fa0b29826
Tracy Dean Blackadder <tracey.blackadder@realestate.com.au>
2019-09-20 17:11:21 +1000

    Commit update 2

--- main.js
+++ main.js
@@ -5,3 +5,6 @@ const sixthCommitOnMaster = 6;
 const seventhCommitOnMaster = 7;
 const update = 'After commit 6 and 7, pulled down origin master.  Commits 4 and 5 on branch still not pushed, but commited.'
 const fourthCommitOnBranchOne = 4;
+
+
+const update2 = 'Tried to merge commit 4 using SourceTree, I think things got merged.  After resvoling conflicts, I checked out branchOne and did git rebase master.'
