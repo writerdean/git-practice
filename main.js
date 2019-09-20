f03a142cad85a3bd2d2470d9a23e090f76fcfa84
Tracy Dean Blackadder <tracey.blackadder@realestate.com.au>
2019-09-20 17:08:37 +1000

    I don't know what's going on

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 const firstCommit = 1;
 const secondCommit = 2;
 const thirdCommit = 3;
-<<<<<<< HEAD
 const sixthCommitOnMaster = 6;
 const seventhCommitOnMaster = 7;
 const update = 'After commit 6 and 7, pulled down origin master.  Commits 4 and 5 on branch still not pushed, but commited.'
-=======
 const fourthCommitOnBranchOne = 4;
->>>>>>> 3b3103cdc829934a2b37babcc16a1419f0e56408
