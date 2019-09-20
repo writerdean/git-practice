31eb98f9285dd3b8e263cdff8dfe428d98c9e78c
Tracy Dean Blackadder <tracey.blackadder@realestate.com.au>
2019-09-20 17:03:11 +1000

    update

--- main.js
+++ main.js
@@ -3,3 +3,4 @@ const secondCommit = 2;
 const thirdCommit = 3;
 const sixthCommitOnMaster = 6;
 const seventhCommitOnMaster = 7;
+const update = 'After commit 6 and 7, pulled down origin master.  Commits 4 and 5 on branch still not pushed, but commited.'
